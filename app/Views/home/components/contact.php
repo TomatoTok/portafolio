8fad9a8db33ec96d8f5a7efe8e269801743863ed
Tom <77556113+TomatoTok@users.noreply.github.com>
2023-07-13 23:07:44 -0300

    contact
    Se agrego una seccion de contacto que permite enviar un correo o copiar la direccion del correo

--- app/Views/home/components/contact.php
+++ app/Views/home/components/contact.php
@@ -3,6 +3,15 @@
         <p class="section-subtitle">¿Cómo te encuentro?</p>
         <h6 class="section-title mb-5">¡Contáctame!</h6>
         <!-- contact form -->
+        <div class="row d-flex justify-content-center ">
+            <div class="col-sm-6 col-xs-12">
+                <small><label class="section-subtitle"> Presiona el correo para enviarme un Mail o presiona "Copiar" para pegarlo en el portapapeles</label></small>
+                <a id="" href="mailto:tom.bascal@gmail.com" class="form-control text-center contact-input" style="background-color:#695aa6" title="Presiona para contactarme" disabled>tom.bascal@gmail.com</a>
+                <input type="text" name="correo" id="correo" value="tom.bascal@gmail.com" style="opacity:0;" hidden>
+            <button type="button" class="btn btn-primary mt-1" style="border-radius:5px;" id="copiar" title="Copiar a portapapeles">Copiar <i class="fas fa-copy"></i></button>
+            </div>
+        </div>
+        <?php if(false){ ?>
         <form action="" class="contact-form col-md-10 col-lg-8 m-auto">
             <div class="form-row">
                 <div class="form-group col-sm-6">
@@ -19,5 +28,20 @@
                 </div>
             </div>
         </form><!-- end of contact form -->
+        <?php } ?>
     </div><!-- end of container -->
 </section>
+
+<script>
+    document.getElementById('copiar').addEventListener("click",()=> {
+        const copy = document.getElementById("correo").value ;
+        document.getElementById('correo').hidden = false;
+        document.getElementById('correo').disabled = false;
+        document.getElementById('correo').focus();
+        document.execCommand('selectAll');
+        document.execCommand('copy');
+        document.getElementById('correo').disabled = true;
+        document.getElementById('correo').hidden = true;
+        alert('Correo copiado al portapapeles');
+    });
+</script>
